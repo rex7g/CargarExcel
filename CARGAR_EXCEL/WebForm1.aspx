4bf15ce4aec97013fe1e19ae888b7712715d9bde
Renato D <Renato D@DESKTOP-H4ICEER>
2022-08-05 15:17:08 -0400

    Add project files.

---
+++ CARGAR_EXCEL/WebForm1.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CARGAR_EXCEL.WebForm1" %>
+
+<%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <style>
+        .mitabla {
+            width :100%
+        }
+    </style>
+</head>
+<body>
+   
+    <form id="form1" runat="server">
+         <div class="container-fluid mt-4">
+                 <div class="card">
+                  <div class="card-header">
+                    Cargar Archivo
+                  </div>
+                  <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="form-row">
+                                <div class="form-group col-sm-10">
+                                  <label for="FileUpload1">Archivo</label>
+                                    <asp:FileUpload ID="FileUpload1" CssClass="form-control-file" runat="server" />
+                                </div>
+                                <div class="form-group col-sm-2">
+                                  <asp:Button ID="Button1" runat="server" Text="Cargar" CssClass="btn btn-block btn-success mt-4" OnClick="Button1_Click" />
+<%--                                  <asp:Button ID="Button2" runat="server" Text="Prueba" CssClass="btn btn-block btn-success mt-4" OnClick="Button2_Click" />--%>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                      <hr />
+                   <div class="row">
+                       <div class="col-sm-12">
+                           <div class="card">
+                          <div class="card-header p-1">
+                            Registros Cargados
+                          </div>
+                          <div class="card-body">
+                              <asp:GridView ID="GridView1" runat="server" CssClass="table table-bordered mitabla table-hover"></asp:GridView>
+                          </div>
+                        </div>
+                     </div>
+                       
+
+                   </div>
+                  </div>
+                </div>
+        </div>
+
+        
+        <%--<div>
+            
+            <br />
+            <br />
+           
+        </div>
+        <br />
+        <div>
+            <asp:Label ID="lblrespuesta" runat="server"></asp:Label>
+        </div>--%>
+        
+    </form>
+</body>
+</html>
---
+++ CARGAR_EXCEL/WebForm1.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+using System.Globalization;
+
+namespace CARGAR_EXCEL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Globalization.CultureInfo.CurrentCulture = new CultureInfo("es-PE");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string ruta_carpeta = HttpContext.Current.Server.MapPath("~/Temporal");
+
+            if (!Directory.Exists(ruta_carpeta))
+            {
+                Directory.CreateDirectory(ruta_carpeta);
+            }
+
+            //GUARDAMOS EL ARCHIVO EN LOCAL
+            var ruta_guardado = Path.Combine(ruta_carpeta, FileUpload1.FileName);
+            FileUpload1.SaveAs(ruta_guardado);
+
+
+            IWorkbook MiExcel = null;
+            FileStream fs = new FileStream(ruta_guardado, FileMode.Open, FileAccess.Read);
+
+            if (Path.GetExtension(ruta_guardado) == ".xlsx")
+                MiExcel = new XSSFWorkbook(fs);
+            else
+                MiExcel = new HSSFWorkbook(fs);
+
+
+            ISheet hoja = MiExcel.GetSheetAt(0);
+
+            DataTable table = new DataTable();
+            table.Columns.Add("data1", typeof(string));
+            table.Columns.Add("data2", typeof(string));
+            table.Columns.Add("data3", typeof(string));
+            table.Columns.Add("data4", typeof(string));
+
+            if (hoja != null) {
+
+                int cantidadfilas = hoja.LastRowNum;
+
+                for (int i = 1; i <= cantidadfilas; i++) {
+                    IRow fila = hoja.GetRow(i);
+
+
+                    if(fila != null)
+                        table.Rows.Add(
+                            fila.GetCell(0, MissingCellPolicy.RETURN_NULL_AND_BLANK) != null ? fila.GetCell(0, MissingCellPolicy.RETURN_NULL_AND_BLANK).NumericCellValue.ToString() : "",
+                            fila.GetCell(1, MissingCellPolicy.RETURN_NULL_AND_BLANK) != null ? fila.GetCell(1, MissingCellPolicy.RETURN_NULL_AND_BLANK).ToString() : "",
+                            fila.GetCell(2, MissingCellPolicy.RETURN_NULL_AND_BLANK) != null  ? fila.GetCell(2, MissingCellPolicy.RETURN_NULL_AND_BLANK).DateCellValue.ToString("dd/MM/yyyy",new CultureInfo("es-ES")) : "",
+                             fila.GetCell(3, MissingCellPolicy.RETURN_NULL_AND_BLANK) != null ? fila.GetCell(3, MissingCellPolicy.RETURN_NULL_AND_BLANK).NumericCellValue.ToString() : ""
+                            );
+                }
+            }
+
+            GridView1.DataSource = table;
+            GridView1.DataBind();
+          
+            int resultado = cargarEnSQL(table);
+
+            if (resultado == 1) {
+                GridView1.DataSource = table;
+                GridView1.DataBind();
+            }
+
+        }
+
+
+
+
+        public int cargarEnSQL(DataTable tabla)
+        {
+            int resultado = 0;
+            try
+            {
+                //NOS CONECTAMOS CON LA BASE DE DATOS
+                using (SqlConnection cn = new SqlConnection("Data Source=.;Initial Catalog=Pruebas ;Integrated Security=True"))
+                {
+                    SqlCommand cmd = new SqlCommand("usp_cargarInformacion", cn);
+                    cmd.Parameters.Add("EstructuraCarga", SqlDbType.Structured).Value = tabla;
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    resultado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                string mensaje = ex.Message.ToString();
+                resultado = 0;
+            }
+
+            return resultado;
+        }
+
+      
+    }
+
+    public class informacion {
+        public string data1 { get; set; }
+        public string data2 { get; set; }
+        public string data3 { get; set; }
+        public string data4 { get; set; }
+    }
+}
